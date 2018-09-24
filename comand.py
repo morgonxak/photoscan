59c7ef4fef47d40c5f48f0eb0a3035c409385cdb
morgonxak <cmit.dima@gmail.com>
2018-09-24 13:15:02 +0700

    comit1

--- comand.py
+++ comand.py
@@ -1,6 +1,8 @@
 import PhotoScan
 import socket
 from threading import Thread
+import json
+import os
 
 def openPachDir():
     '''
@@ -8,22 +10,6 @@ def openPachDir():
     :return:
     '''
     return PhotoScan.app.getExistingDirectory("Dosier images avec Exif:")
-
-def AddPhoto(chunk, patchDirImage):
-    '''
-    patchDirImage: Путь до папки с изображениями
-    :param chunk
-    :param patchDirImage r"F:\18.08.02 Шерегеш\Фото\1 флешка"
-    :return:
-    '''
-    import os
-    path_photos = patchDirImage
-    image_list = os.listdir(path_photos)
-    photo_list = list()
-    for photo in image_list:
-        if photo.rsplit(".", 1)[1].lower() in ["jpg", "jpeg", "tif", "tiff"]:
-            photo_list.append("/".join([path_photos, photo]))
-    chunk.addPhotos(photo_list)
 
 def creatProject(project_path, project_name):
     '''
@@ -41,8 +27,23 @@ def creatProject(project_path, project_name):
 
     return chunk, doc
 
-def alingPhotos(accuracy=PhotoScan.HighAccuracy, generic_preselection=True, reference_preselection=True,
-                  filter_mask=False, keypoint_limit=350000, tiepoint_limit=0):
+def AddPhoto(chunk, patchDirImage):
+    '''
+    patchDirImage: Путь до папки с изображениями
+    :param chunk
+    :param patchDirImage r"F:\18.08.02 Шерегеш\Фото\1 флешка"
+    :return:
+    '''
+    import os
+    path_photos = patchDirImage
+    image_list = os.listdir(path_photos)
+    photo_list = list()
+    for photo in image_list:
+        if photo.rsplit(".", 1)[1].lower() in ["jpg", "jpeg", "tif", "tiff"]:
+            photo_list.append("/".join([path_photos, photo]))
+    return chunk.addPhotos(photo_list)
+
+def alingPhotos(chunk, doc):
     '''
     выровнить фотографии
     :param accuracy:
@@ -53,31 +54,34 @@ def alingPhotos(accuracy=PhotoScan.HighAccuracy, generic_preselection=True, refe
     :param tiepoint_limit:
     :return:
     '''
-    chunk.matchPhotos(accuracy, generic_preselection, reference_preselection,filter_mask, keypoint_limit, tiepoint_limit)
+    chunk.matchPhotos(accuracy=PhotoScan.HighAccuracy, generic_preselection=True, reference_preselection=True,
+                  filter_mask=False, keypoint_limit=350000, tiepoint_limit=0)
     chunk.alignCameras()
     doc.save()
 
-def setCoordinateSystem(CK = "EPSG::4326"):
+def setCoordinateSystem(chunk, doc):
     '''
     Установить систему координат
     :return:
     '''
-    chunk.crs = PhotoScan.CoordinateSystem(CK)
+    chunk.crs = PhotoScan.CoordinateSystem(CK = "EPSG::4326")
     chunk.updateTransform()
     doc.save()
 
-def buildDenseCloud(quality=PhotoScan.UltraQuality, filter=PhotoScan.AggressiveFiltering):
+def buildDenseCloud(chunk, doc):
     '''
     строить плотное облако
     :param quality:
     :param filter:
     :return:
     '''
-    chunk.buildDenseCloud(quality=PhotoScan.UltraQuality, filter=PhotoScan.AggressiveFiltering)
+    #chunk.buildDenseCloud(quality=PhotoScan.UltraQuality, filter=PhotoScan.AggressiveFiltering)
+
+    chunk.buildDepthMaps(quality=PhotoScan.UltraQuality, filter=PhotoScan.AggressiveFiltering)
+    chunk.buildDenseCloud(point_colors=True)
     doc.save()
 
-def exportPoints(project_path, project_name,binary=True, precision=6, colors=True,
-                   format=PhotoScan.PointsFormatLAS):
+def exportPoints(chunk, project_path, project_name):
     '''
     Экспорт облако точек
     :param project_path: Путь до папки для экспорта
@@ -91,8 +95,7 @@ def exportPoints(project_path, project_name,binary=True, precision=6, colors=Tru
     chunk.exportPoints(project_path + project_name + ".las", binary=True, precision=6, colors=True,
                    format=PhotoScan.PointsFormatLAS)
 
-def buildModel(surface=PhotoScan.HeightField, interpolation=PhotoScan.EnabledInterpolation,
-                 face_count=PhotoScan.MediumFaceCount):
+def buildModel(chunk, doc):
     '''
     Создать модель
     :param surface:
@@ -100,21 +103,21 @@ def buildModel(surface=PhotoScan.HeightField, interpolation=PhotoScan.EnabledInt
     :param face_count:
     :return:
     '''
-    chunk.buildModel(surface, interpolation, face_count)
+    chunk.buildModel(surface=PhotoScan.HeightField, interpolation=PhotoScan.EnabledInterpolation,
+                 face_count=PhotoScan.MediumFaceCount)
     doc.save()
 
-def buildDEM(source=PhotoScan.DenseCloudData, interpolation=PhotoScan.EnabledInterpolation):
+def buildDEM(chunk, doc):
     '''
     Построить карту высот
     :param source:
     :param interpolation:
     :return:
     '''
-    chunk.buildDem(source, interpolation)
+    chunk.buildDem(source=PhotoScan.DenseCloudData, interpolation=PhotoScan.EnabledInterpolation)
     doc.save()
 
-def exportDem(project_path, project_name, image_format=PhotoScan.ImageFormatTIFF,
-                format=PhotoScan.RasterFormatTiles, nodata=-32767, write_kml=False, write_world=True):
+def exportDem(chunk, project_path, project_name):
     '''
     Экспортировать карту высот
     :param project_path:
@@ -126,10 +129,10 @@ def exportDem(project_path, project_name, image_format=PhotoScan.ImageFormatTIFF
     :param write_world:
     :return:
     '''
-    chunk.exportDem(project_path + project_name + "_DEM.tif", image_format,
-                format, nodata, write_kml, write_world)
+    chunk.exportDem(project_path + project_name + "_DEM.tif", image_format=PhotoScan.ImageFormatTIFF,
+                format=PhotoScan.RasterFormatTiles, nodata=-32767, write_kml=False, write_world=True)
 
-def buildOrtho(surface=PhotoScan.ElevationData, blending=PhotoScan.MosaicBlending, color_correction=False):
+def buildOrtho(chunk, doc):
     '''
     Построить ортофотоплан
     :param surface:
@@ -137,12 +140,12 @@ def buildOrtho(surface=PhotoScan.ElevationData, blending=PhotoScan.MosaicBlendin
     :param color_correction:
     :return:
     '''
-    chunk.buildOrthomosaic(surface, blending, color_correction)
+    #chunk.buildOrthomosaic(surface=PhotoScan.ElevationData, blending=PhotoScan.MosaicBlending)
+    chunk.buildOrthomosaic(surface=PhotoScan.ElevationData, blending=PhotoScan.MosaicBlending, color_correction=True, fill_holes=True)
+
     doc.save()
 
-def exportOrthomosaic(project_path, project_name, image_format=PhotoScan.ImageFormatTIFF,
-                        format=PhotoScan.RasterFormatTiles, raster_transform=PhotoScan.RasterTransformNone,
-                        write_kml=False, write_world=True):
+def exportOrthomosaic(chunk, project_path, project_name):
     '''
     Экспорт ортофотоплана
     :param project_path: Папка
@@ -154,41 +157,105 @@ def exportOrthomosaic(project_path, project_name, image_format=PhotoScan.ImageFo
     :param write_world:
     :return:
     '''
-    chunk.exportOrthomosaic(project_path + project_name + ".tif", image_format, format, raster_transform, write_kml,write_world )
+    chunk.exportOrthomosaic(project_path + project_name + ".tif", image_format=PhotoScan.ImageFormatTIFF,
+                        format=PhotoScan.RasterFormatTiles, raster_transform=PhotoScan.RasterTransformNone,
+                        write_kml=False, write_world=True)
 
-def dispatch(self, value):
-    method_name = str(value)
-    method = getattr(self, method_name)
-    return method()
+
+PATH_DIR = r'D:\dimaProject\photoscan\processing_photoscan'
 
 class ConnectServer(Thread):
     def __init__(self, addr):
         self.addr = addr
 
+    def parserData(self, quiry):
+        '''
+        парсит запрос на функцию которую необходимо вызвать и ее аргументы
+        :param quiry:
+        :return:
+        '''
+        quiry = quiry.decode('utf-8')
+        return json.loads(quiry)
+
+    def sendMessage(self, message):
+
+        self.sock.send(bytes(message, encoding='utf-8'))
+
     def run(self):
         self.startClient()
 
     def startClient(self):
-        sock = socket.socket()
-        sock.connect(self.addr)
+        '''
+        Должен подключится к серверу и ждать
+        :return:
+        '''
+        self.sock = socket.socket()
+        self.sock.connect(self.addr)
 
-        while True:
-            #sock.settimeout(60)  # установка таймаута
-            #sock.setblocking(0)
-            data = sock.recv(1024)
-            print("данные с сервера", data)
-            #dispatch(data)
-            if data == b'stop':
-                break
-            if data == b'creat':
-                creatProject(r"D:\dimaProject\photoscan\processing_photoscan\ID_1", "test")
-        sock.close()
+
+        print("Ждем ответа от сервера и его параметры")
+        data = self.sock.recv(1024)
+
+
+        data = self.parserData(data)
+        print("Аргументы сервера", data)
+        ###
+        #то что должно быть сделанно
+        self.chunk, self.doc = creatProject(PATH_DIR + '\/' + data["ID_User"] + '\/', 'project')
+        AddPhoto(self.chunk, PATH_DIR + '\/' + data["ID_User"]+'\/'+'photo')
+
+        alingPhotos(self.chunk, self.doc)  #Выровнить фотографии
+        message = "Выравнивание фотографий -- OK"
+        print(message)
+        self.sendMessage(message)
+
+        setCoordinateSystem(self.chunk, self.doc)  #Выстовить СК
+        message = "Выстовления системы координат -- OK"
+        print(message)
+        self.sendMessage(message)
+
+        buildDenseCloud(self.chunk, self.doc)  #строить плотное облако
+        message = "Построения плотного облако точек -- OK"
+        print(message)
+        self.sendMessage(message)
+
+        buildModel(self.chunk, self.doc)  #Строим модель
+        message = "Построения модели -- OK"
+        print(message)
+        self.sendMessage(message)
+
+        buildDEM(self.chunk, self.doc)  #Строим Цифровую модель рельефа
+        message = "Построения рельефа -- OK"
+        print(message)
+        self.sendMessage(message)
+
+        #buildOrtho(self.chunk, self.doc)  #Строим ортофотоплан
+        message = "Построения ортофотоплана -- OK"
+        print(message)
+        self.sendMessage(message)
+
+        ##кспортируем все по папкам
+        exportDem(self.chunk, PATH_DIR + '\/' + data["ID_User"]+'\/'+'DEM' + '\/', 'DEM')
+        message = "Экспорт DEM -- OK"
+        print(message)
+        self.sendMessage(message)
+
+        #exportOrthomosaic(self.chunk, PATH_DIR + '\/' + data["ID_User"]+'\/'+'orthophoto' + '\/', 'orthophoto')
+        message = "Экспорт ортофотоплана -- OK"
+        print(message)
+        self.sendMessage(message)
+
+        ###
+        #Отправка отчета
+        message = 'vse'
+        self.sendMessage(message)
+        self.sock.close()
         print("соеденения закрыто")
-
 
 host = 'localhost'
 port = 777
 addr = (host, port)
 
 ThreadSoket = ConnectServer(addr)
+
 ThreadSoket.run()
